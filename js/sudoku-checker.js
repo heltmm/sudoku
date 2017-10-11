8bf1e0ccc4cb402595ae6c2dfc1b3ae8e2411f70
Keegan Ruebling and Mark Helt <keegan+mark@>
2017-10-10 17:09:15 -0700

    create columns

--- js/sudoku-checker.js
+++ js/sudoku-checker.js
@@ -1,23 +1,14 @@
 function Sudoku(array) {
-  this.puzzle = [[1,2,3,4,5,6,7,8,9],
-                [2,3,4,5,6,7,8,9,1],
-                [3,4,5,6,7,8,9,1,2],
-                [4,5,6,7,8,9,1,2,3],
-                [5,6,7,8,9,1,2,3,4],
-                [6,7,8,9,1,2,3,4,5],
-                [7,8,9,1,2,3,4,5,6],
-                [8,9,1,2,3,4,5,6,7],
-                [9,1,2,3,4,5,6,7,8]];
+  this.puzzle = array;
 }
-
 
 
 Sudoku.prototype.checker = function() {
   var checker = [1,2,3,4,5,6,7,8,9];
 
-  for(let i = 0; i < 9; i++){
+  for(var i = 0; i < 9; i++){
     sorted = this.puzzle[i].sort()
-    for(let j = 0; j < 9; j++){
+    for(var j = 0; j < 9; j++){
       if(sorted[j] != checker[j]){
         return false;
       };
@@ -25,3 +16,33 @@ Sudoku.prototype.checker = function() {
   };
   return true;
 };
+
+Sudoku.prototype.createColumns = function(){
+  var columns = [[],[],[],[],[],[],[],[],[]]
+  for(var i = 0; i < 9; i++){
+    for(var j = 0; j < 9; j++){
+      columns[i].push(this.puzzle[i][j])
+    }
+  }
+  return columns
+}
+
+Sudoku.prototype.isCorrect = function() {
+  if self.checker() === false {
+    return false
+  }
+  new_sudoku = new Sudoku(self.createColumns())
+
+  if new_sudoku.checker() === false {
+    return false
+  }
+}
+array = [[1,2,3,4,5,6,7,8,9],
+        [2,3,4,5,6,7,8,9,1],
+        [3,4,5,6,7,8,9,1,2],
+        [4,5,6,7,8,9,1,2,3],
+        [5,6,7,8,9,1,2,3,4],
+        [6,7,8,9,1,2,3,4,5],
+        [7,8,9,1,2,3,4,5,6],
+        [8,9,1,2,3,4,5,6,7],
+        [9,1,2,3,4,5,6,7,8]]
